bd5de1e30653989f56381821b2a3804175166073
Luiza Nigogosova <luiza.nigogosova@leroymerlin.ru>
2023-10-16 22:12:56 +0300

    config for night

--- hw_asr/model/deep_speech_model.py
+++ hw_asr/model/deep_speech_model.py
@@ -2,26 +2,32 @@ from torch import nn
 from hw_asr.base import BaseModel
 
 class DeepSpeechModel(BaseModel):
-    def __init__(self, n_feats, n_class, rnn_layers=5, rnn_hidden_size=512, **batch):
+    def __init__(self, n_feats, n_class, rnn_layers=5, rnn_hidden_size=512, dropout=0.2, **batch):
         super().__init__(n_feats, n_class, **batch)
 
         self.conv = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=(41, 11), stride=(2, 2), padding=(20, 5)),
             nn.BatchNorm2d(32),
             nn.Hardtanh(0, 20),
+            nn.Dropout(dropout),
             nn.Conv2d(32, 32, kernel_size=(21, 11), stride=(2, 1), padding=(10, 5)),
             nn.BatchNorm2d(32),
             nn.Hardtanh(0, 20),
+            nn.Dropout(dropout)
         )
         conv_output_size = (n_feats + 2 * 5 - (11 - 1) - 1) // 2 + 1
         conv_output_size = (conv_output_size + 2 * 5 - (11 - 1) - 1) // 1 + 1
 
-        self.rnn = nn.GRU(
-            input_size=conv_output_size,
-            hidden_size=rnn_hidden_size,
-            num_layers=rnn_layers,
-            bidirectional=True,
-            batch_first=True,
+        self.rnn = nn.Sequential(
+            nn.GRU(
+                input_size=conv_output_size,
+                hidden_size=rnn_hidden_size,
+                num_layers=rnn_layers,
+                bidirectional=True,
+                batch_first=True,
+            ),
+            #nn.BatchNorm2d(rnn_hidden_size),
+            #nn.Dropout(dropout)
         )
 
         self.fc = nn.Linear(rnn_hidden_size * 2, n_class)
@@ -29,13 +35,13 @@ class DeepSpeechModel(BaseModel):
     def forward(self, spectrogram, **batch):
         #print("====start====")
         #print(spectrogram.shape)
-        #spectrogram = spectrogram.unsqueeze(1).transpose(2, 3) # Batch x Channels(1) x Time x Embed
+        spectrogram = spectrogram.unsqueeze(1).transpose(2, 3) # Batch x Channels(1) x Time x Embed
         #print(spectrogram.shape)
 
-        output = self.conv(spectrogram.unsqueeze(1).transpose(2, 3)).flatten(1, 2)
+        output = self.conv(spectrogram)
         #print("Conv", output.shape)
 
-        #output = output.flatten(1, 2) # Batch x Сhannels * Dimension (Sequence) x Embed
+        output = output.flatten(1, 2) # Batch x Сhannels * Dimension (Sequence) x Embed
         #print(output.shape)
         output, _ = self.rnn(output)
         #print("Rnn", output.shape)
