1afd3869b045b8138b85dff2c062789616320e5d
Luiza Nigogosova <luiza.nigogosova@leroymerlin.ru>
2023-10-16 10:57:58 +0300

    fix deepspeech

--- hw_asr/model/deep_speech_model.py
+++ hw_asr/model/deep_speech_model.py
@@ -6,16 +6,18 @@ class DeepSpeechModel(BaseModel):
         super().__init__(n_feats, n_class, **batch)
 
         self.conv = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=(11, 41), stride=(1, 2), padding=(5, 20)),
+            nn.Conv2d(1, 32, kernel_size=(41, 11), stride=(2, 2), padding=(20, 5)),
             nn.BatchNorm2d(32),
             nn.Hardtanh(0, 20),
-            nn.Conv2d(32, 32, kernel_size=(11, 21), stride=(1, 2), padding=(5, 10)),
+            nn.Conv2d(32, 32, kernel_size=(21, 11), stride=(2, 1), padding=(10, 5)),
             nn.BatchNorm2d(32),
             nn.Hardtanh(0, 20),
         )
+        conv_output_size = (n_feats + 2 * 5 - (11 - 1) - 1) // 2 + 1
+        conv_output_size = (conv_output_size + 2 * 5 - (11 - 1) - 1) // 1 + 1
 
         self.rnn = nn.GRU(
-            input_size=n_feats * 32 // 2 // 2,
+            input_size=conv_output_size,
             hidden_size=rnn_hidden_size,
             num_layers=rnn_layers,
             bidirectional=True,
@@ -27,12 +29,16 @@ class DeepSpeechModel(BaseModel):
     def forward(self, spectrogram, **batch):
         #print("====start====")
         #print(spectrogram.shape)
-        spectrogram = spectrogram.transpose(1, 2).unsqueeze(1) # Batch x 1(channel) x Embed x Time
+        spectrogram = spectrogram.unsqueeze(1).transpose(2, 3) # Batch x 1(channel) x Embed x Time
         #print(spectrogram.shape)
 
         output = self.conv(spectrogram)
         #print("Conv", output.shape)
-        output = output.transpose(1, 2).flatten(2, 3)
+        batch_size, channels, dimension, seq_lengths = output.size()
+
+        #output = output.permute(0, 3, 1, 2)
+        #output = output.view(batch_size, seq_lengths, channels * dimension)
+        output = output.flatten(1, 2)
         #print(output.shape)
         output, _ = self.rnn(output)
         #print("Rnn", output.shape)
